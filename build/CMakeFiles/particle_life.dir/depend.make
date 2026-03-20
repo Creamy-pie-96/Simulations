054bc6dc162163d78f2bf3c755a85f6e5fd1f16f
Creamy-pie-96 <nasifsadik9@gmail.com>
2026-03-20 08:22:23 +0600

    Remove build directory from repository

--- build/CMakeFiles/particle_life.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for particle_life.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/particle_life.dir/src/Particle.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/particle_life.dir/src/Particle.cpp.o: \
- /home/DATA/CODE/code/particle_life/src/Particle.cpp \
- /usr/include/stdc-predef.h
